77f731b332ff9c4c3a618d765bb64ae2b4786232
Pontus Thomé <Pontus.Thome@hiq.se>
2019-03-25 19:21:36 +0100

    Trying to create a pendingRequest with firebase functions

--- firebase_functions/functions/src/index.ts
+++ firebase_functions/functions/src/index.ts
@@ -1,8 +1,16 @@
 import * as functions from 'firebase-functions';
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+//  const original = request.query.text;
+
+ // response.send("Hello from Firebase!" + original);
+
+    admin.firestore().collection('pendingRequeest').add({HpID: "testId", pnr: "1912121212"});
+
+    return response.json({result: `Message with ID: added.`});
+});
