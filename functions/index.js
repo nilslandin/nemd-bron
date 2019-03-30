ad6dabf438f4cb7db169059797877f278d0bf41f
nillan <nils.landin@zenuity.com>
2019-03-30 19:38:18 +0100

    Expanded addPendingRequest.

--- functions/index.js
+++ functions/index.js
@@ -4,13 +4,24 @@ admin.initializeApp();
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.addPendingRequest = functions.https.onRequest((request, response) => {
+exports.addPendingRequestAndNotifyPatient = functions.https.onRequest((request, response) => {
 
  	return admin.firestore().collection("pendingRequests").add({
 		ssn: request.query.ssn, 
 		hpid: request.query.hpid
 	}).then((result) => {
-	 	return response.send("Hello from Firebase!" + result.id + " end");
-		// body...
+		return	admin.firestore().collection("users").where('ssn', '==', request.query.ssn).get().then(snapshot => {
+			if (snapshot.empty) {
+	 			return response.send("No matching documents");
+			}
+
+			var user;
+		    snapshot.forEach(doc => {
+		    	user = doc;
+			});
+
+			return response.send("Got user from Firebase!");
+		})
 	})
  });
+
