d21c01c269567ab7e72c94c534e735101ae8d737
nillan <nils.landin@zenuity.com>
2019-03-30 15:05:51 +0100

    Expanded pendingRequest addition

--- functions/index.js
+++ functions/index.js
@@ -9,10 +9,7 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 
     admin.firestore().collection('pendingRequest').add({hpid: request.query.hpid,
     	ssn: request.query.ssn, accepted: false, declined: false, acceptedhps: []})
-    .then(function (result) {
-    	return response.json({result2: "Document written with ID: " + result.id + " end"}
-    })
-
+    
     //return response.json({result: `Message with ID: added.`});
 });
 export const helloWorld2 = functions.https.onRequest((request, response) => {
